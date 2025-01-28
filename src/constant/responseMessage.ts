818a2d8da5fa9d21e6b2cf73fc241df39a2874e7
amithsulakhe <amithsulakhe729@gmail.com>
2025-01-28 19:57:37 +0530

    chore: Dependency update

--- src/constant/responseMessage.ts
+++ src/constant/responseMessage.ts
@@ -1,5 +1,6 @@
 export default {
     SUCCESS: 'The operation has been successfull',
     SOMETHING_WENT_WRONG: 'something went wrong',
-    NOT_FOUND: (entity: string) => `${entity} not found`
+    NOT_FOUND: (entity: string) => `${entity} not found`,
+    TOO_MANY_REQUESTS: `Too many requests! Please try again after some time`
 }
--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 import apiController from '../controller/apiController'
+import rateLimit from '../middleware/rateLimit'
 
 const router = Router()
 
-router.route('/self').get(apiController.self)
+// for all end point after 10 time api call it will throw error
+// router.use(rateLimit)
+
+router.route('/self').get(rateLimit, apiController.self)
 router.route('/health').get(apiController.health)
 
 export default router
